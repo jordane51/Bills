93862192a27028a2fecf87a9bc896539b79c481f
xhitedev <e.greg.bx@gmail.com>
2015-12-23 20:02:35 +0100

    switched to mongoose

--- client/index.js
+++ client/index.js
@@ -5,6 +5,8 @@
 var express = require('express');
 var app = module.exports = express();
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
